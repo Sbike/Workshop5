bdaae1da2e03c4bb50859b4b80d5a4f810aea535
hubert leroy <hubert.leroy@edu.devinci.fr>
2024-03-10 22:45:16 +0100

    Testing stuff to complete the next tests, connection problemto port 3000.

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -2,22 +2,24 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
 import { Value } from "../types";
+import { delay } from "../utils";
+import axios from 'axios';
 
 export async function node(
-  nodeId: number, // the ID of the node
-  N: number, // total number of nodes in the network
-  F: number, // number of faulty nodes in the network
-  initialValue: Value, // initial value of the node
-  isFaulty: boolean, // true if the node is faulty, false otherwise
-  nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
-  setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
+    nodeId: number, // the ID of the node
+    N: number, // total number of nodes in the network
+    F: number, // number of faulty nodes in the network
+    initialValue: Value, // initial value of the node
+    isFaulty: boolean, // true if the node is faulty, false otherwise
+    nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
+    setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
 ) {
+  let x = initialValue;
+  let k = 0;
+
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
-
-  // TODO implement this
-  // this route allows retrieving the current status of the node
 
   node.get("/status", (req, res) => {
     if (isFaulty) {
@@ -27,30 +29,96 @@ export async function node(
     }
   });
 
-  // TODO implement this
-  // this route allows the node to receive messages from other nodes
-  // node.post("/message", (req, res) => {});
+  const messages: Record<number, any[]> = {};
 
-  // TODO implement this
-  // this route is used to start the consensus algorithm
-  // node.get("/start", async (req, res) => {});
+  async function sendMessage(recipient: number, message: any) {
+    if (!nodesAreReady()) {
+      throw new Error('Not all nodes are ready');
+    }
+    await axios.post(`http://localhost:${BASE_NODE_PORT + recipient}/message`, message);
 
-  // TODO implement this
-  // this route is used to stop the consensus algorithm
-  // node.get("/stop", async (req, res) => {});
+  }
 
-  // TODO implement this
-  // get the current state of a node
-  // node.get("/getState", (req, res) => {});
+  async function waitForMessages(k: number, count: number) {
+    const startTime = Date.now();
+    while (!messages[k] || messages[k].length < count) {
+      if (Date.now() - startTime > 5000) {
+        return false;
+      }
+      await delay(1000);
+    }
+    return true;
+  }
 
-  // start the server
-  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
-    console.log(
-      `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
-    );
+  async function startRound() {
+    while (true) {
+      k++;
+      for (let i = 0; i < N; i++) {
+        if (i !== nodeId) {
+          await sendMessage(i, { k, type: 'R', value: x });
+        }
+      }
 
-    // the node is ready
+      const receivedR = await waitForMessages(k, N - F);
+      if (receivedR) {
+        const v = messages[k].find(msg => msg.type === 'R').value;
+        const moreThanHalf = messages[k].filter(msg => msg.type === 'R' && msg.value === v).length > N / 2;
+        for (let i = 0; i < N; i++) {
+          if (i !== nodeId) {
+            await sendMessage(i, { k, type: 'P', value: moreThanHalf ? v : '?' });
+          }
+        }
+      }
+
+      const receivedP = await waitForMessages(k, N - F);
+      if (receivedP) {
+        const v = messages[k].find(msg => msg.type === 'P' && msg.value !== '?').value;
+        const atLeastFPlusOne = messages[k].filter(msg => msg.type === 'P' && msg.value === v).length >= F + 1;
+        if (atLeastFPlusOne) {
+          console.log(`Node ${nodeId} decided on value ${v}`);
+        }
+        if (v) {
+          x = v;
+        } else {
+          x = Math.random() < 0.5 ? 0 : 1;
+        }
+      }
+    }
+  }
+
+  node.post("/message", async (req, res) => {
+    const { round: messageRound, type, value } = req.body;
+    if (!messages[k]) {
+      messages[k] = [];
+    }
+    messages[k].push({ type, value });
+    res.status(200).json({ k, type, value });
+  });
+
+  node.get("/start", async (req, res) => {
+    if (!nodesAreReady()) {
+      throw new Error('Not all nodes are ready');
+    }
+    startRound();
+    res.status(200).send();
+  });
+
+  const server = node.listen(BASE_NODE_PORT + nodeId, () => {
+    console.log(`Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`);
     setNodeIsReady(nodeId);
+  });
+
+  server.on('listening', () => {
+    const address = server.address();
+    if (typeof address === 'string') {
+      console.log(`Server is listening at ${address}`);
+    } else if (address) {
+      console.log(`Server is listening on port ${address.port}`);
+    }
+  });
+
+  server.on('error', (error) => {
+    console.error(`Error occurred: ${error.message}`);
   });
 
   return server;
