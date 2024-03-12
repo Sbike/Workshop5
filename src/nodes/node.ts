40f1cf118a4c21b2c664d9e650a706363a607c5e
hubert leroy <hubert.leroy@edu.devinci.fr>
2024-03-12 13:56:40 +0100

    3 test passed, still need to find how to pass the others but i should be close

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -2,8 +2,10 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
 import { NodeState, Value } from "../types";
-import { delay } from "../utils";
 import axios from 'axios';
+import * as console from "console";
+import {delay} from "../utils";
+
 
 export async function node(
     nodeId: number, // the ID of the node
@@ -14,9 +16,6 @@ export async function node(
     nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
     setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
 ) {
-  let x = initialValue;
-  let k = 0;
-
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
@@ -37,101 +36,94 @@ export async function node(
     k: null
   };
 
-  const messages: Record<number, any[]> = {};
+  let messagesR: Map<number, any[]> = new Map();
+  let messagesP: Map<number, any[]> = new Map();
 
   node.post("/message", async (req, res) => {
-    const { k, x, messageType } = req.body;
-
-    if (!messages[k]) {
-      messages[k] = [];
-    }
-
-    messages[k].push({ x, messageType });
-
-    if (messageType === "R") {
-      if (messages[k].filter((msg) => msg.messageType === "R").length >= N - F) {
-        const valueCounts = messages[k].reduce((counts, msg) => {
-          counts[msg.x] = (counts[msg.x] || 0) + 1;
-          return counts;
-        }, {});
-
-        let proposedValue = "?";
-        for (const value in valueCounts) {
-          if (valueCounts[value] > N / 2) {
-            proposedValue = value;
-            break;
+    let { k, x, messageType } = req.body;
+    if (!isFaulty && !nodeState.killed && k === nodeState.k) {
+      if (messageType == "R") {
+        if (!messagesR.has(k)) {
+          messagesR.set(k, []);
+        }
+        messagesR.get(k)!.push(x)
+        let messageR = messagesR.get(k)!;
+        if (messageR.length >= N - F) {
+          let count0 = messageR.filter((el) => el == 0).length;
+          let count1 = messageR.filter((el) => el == 1).length;
+          if (count0 > (N / 2)) {
+            x = 0;
+          } else if (count1 > (N / 2)) {
+            x = 1;
+          } else {
+            x = "?";
+          }
+          for (let i = 0; i < N; i++) {
+              await axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {k: k, x: x, messageType: "P"});
           }
         }
-
-        for (let i = 0; i < N; i++) {
-          if (i !== nodeId) {
-            await axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, { k, x: proposedValue, messageType: "P" });
-          }
+      } else if (messageType == "P") {
+        if (!messagesP.has(k)) {
+          messagesP.set(k, []);
         }
-      }
-    } else if (messageType === "P") {
-      if (messages[k].filter((msg) => msg.messageType === "P").length >= N - F) {
-        const valueCounts = messages[k].reduce((counts, msg) => {
-          counts[msg.x] = (counts[msg.x] || 0) + 1;
-          return counts;
-        }, {});
+        messagesP.get(k)!.push(x)
+        let messageP = messagesP.get(k)!;
+        if (messageP.length >= N - F) {
+          console.log("P", messageP,"node :",nodeId,"k :",k)
+          let count0 = messageP.filter((el) => el == 0).length;
+          let count1 = messageP.filter((el) => el == 1).length;
 
-        let decidedValue = null;
-        for (const value in valueCounts) {
-          if (value !== "?" && valueCounts[value] >= F + 1) {
-            decidedValue = value;
-            break;
-          }
-        }
-
-        if (decidedValue !== null) {
-          nodeState.decided = true;
-          nodeState.x = decidedValue as Value;
-        } else {
-          let nonQuestionValue = null;
-          for (const value in valueCounts) {
-            if (value !== "?") {
-              nonQuestionValue = value;
-              break;
+          if (count0 >= F + 1) {
+            nodeState.x = 0;
+            nodeState.decided = true;
+          } else if (count1 >= F + 1) {
+            nodeState.x = 1;
+            nodeState.decided = true;
+          } else {
+            if (count0 + count1 > 0 && count0 > count1) {
+              nodeState.x = 0;
+            } else if (count0 + count1 > 0 && count0 < count1) {
+              nodeState.x = 1;
+            } else {
+              nodeState.x = Math.random() > 0.5 ? 0 : 1;
+            }
+            nodeState.k = k + 1;
+            for (let i = 0; i < N; i++) {
+                await axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {k: k, x: x, messageType: "R"});
             }
           }
-
-          if (nonQuestionValue !== null) {
-            nodeState.x = nonQuestionValue as Value;
-          } else {
-            nodeState.x = Math.random() < 0.5 ? 0 : 1;
-          }
         }
       }
     }
-    res.status(200).send();
+    res.status(200).send("message received");
   });
 
 
-
   node.get("/start", async (req, res) => {
-    if (nodeState.killed) {
-      res.status(500).send("Node is killed");
-      return;
+
+    while (!nodesAreReady()) {
+      await delay(5);
     }
-    if (isFaulty) {
-      nodeState.decided = null;
-      nodeState.x = null;
-        nodeState.k = null;
+
+    if (!isFaulty) {
+      nodeState.decided = false;
+      nodeState.x = initialValue;
+      nodeState.k = 1;
+      //sends a proposal message (R, k, x) to all other processes
+      for (let i = 0; i < N; i++) {
+        await axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+          k: nodeState.k,
+          x: nodeState.x,
+          messageType: "R"
+        });
+      }
     }
     else {
-
+      nodeState.decided = null;
+      nodeState.x = null;
+      nodeState.k = null;
     }
-    nodeState.decided = false;
-    nodeState.x = initialValue;
-    nodeState.k = 1;
-
-    // Send a proposal message (R, k, x) to all other processes
-    for (let i = 0; i < N; i++) {
-        await axios.post(`http://localhost:${BASE_NODE_PORT + i}/message`, { k: nodeState.k, x: nodeState.x, messageType: "R" });
-    }
-
-    res.status(200).send();
+    res.status(200).send("started");
   });
 
   node.get("/stop", (req, res) => {
@@ -140,10 +132,10 @@ export async function node(
   });
 
   node.get("/getState", (req, res) => {
-    res.status(200).send({ x: nodeState.x, k: nodeState.k, isFaulty, decided: nodeState.decided });
+    res.status(200).send({ x: nodeState.x, k: nodeState.k, killed: nodeState.killed, decided: nodeState.decided });
   });
 
-  const server = node.listen(BASE_NODE_PORT + nodeId, () => {
+  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
     console.log(`Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`);
     setNodeIsReady(nodeId);
   });
